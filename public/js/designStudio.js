97b4f2d9621eed4d016e177763ba82b95e2a2f6f
junedlanja <junedlanja@gmail.com>
2018-11-03 20:56:14 +0530

    First commit

---
+++ public/js/designStudio.js
@@ -0,0 +1,98 @@
+//Design studio module
+var DesignStudio = (function() {
+    return {
+        canvas: null,
+
+        defaultHeight : 600,
+        
+        defaultWidth: 600,
+
+        //creates the canvas with specified height and width
+        createCanvas: function(canvasId) {
+            if (!canvasId || !document.getElementById(canvasId))
+                throw new Error('Canvas element not found');
+            this.canvas = new fabric.Canvas(canvasId, {
+                height: this.defaultHeight,
+                width: this.defaultWidth
+            });
+            return this.canvas;
+        },
+
+        //to set background (product image)
+        setBackground: function(_image) {
+            if (!_image)
+                throw new Error('image is undefined');
+
+            var objects = this.canvas.getObjects();
+
+            //if already background is set than remove it
+            var backgroundObj = objects.find(function(obj) {
+                return obj.isCanvasBackground
+            });
+            if (backgroundObj)
+                this.canvas.remove(backgroundObj);
+      
+            var ih = _image.naturalHeight,
+                iw = _image.naturalWidth;
+
+            var image = new fabric.Image(_image);
+            image.isCanvasBackground = true;
+            //background object shouldn't allowed to select or modify
+            image.selectable = false;
+            //scale background image to fit the canvas while maintaining aspect ration
+            (ih >= iw) ? image.scaleToHeight(this.canvas.getHeight()): image.scaleToWidth(this.canvas.getWidth());
+            this.canvas.centerObject(image);
+            this.canvas.add(image);
+        },
+ 
+
+        //add text 
+        addText: function(text) {
+            if (!Boolean(text) || !text.trim().length)
+                throw new Error('text is undefined');
+            var text = new fabric.Text(text, {
+                fill: 'black'
+            });
+            this.canvas.centerObject(text);
+            this.canvas.add(text);
+            this.canvas.renderAll();
+        },
+
+        //add image
+        addImage: function(_image) {
+            if (!_image)
+                throw new Error('image is undefined');
+            var image = new fabric.Image(_image);
+            this.canvas.centerObject(image);
+            this.canvas.add(image);
+            canvas.renderAll();
+        },
+
+        //delete the active/selected object
+        deleteActiveObject: function() {
+            if (!this.canvas)
+                throw new Error('No canvas found');
+            var activeObj = this.canvas.getActiveObject();
+            if (activeObj && !activeObj.isCanvasBackground)
+                this.canvas.remove(activeObj);
+        },
+
+        //clear all the designs from canvas
+        clearDesigns: function() {
+            if (!this.canvas)
+                throw new Error('No canvas found');
+            var objects = this.canvas.getObjects();
+            objects.forEach(function(obj) {
+                //don't remove background image
+                !obj.isCanvasBackground && this.canvas.remove(obj);
+            }.bind(this))
+        },
+
+        //export canvas design as base64 image
+        exportDesign: function() {
+            if (!this.canvas)
+                throw new Error('No canvas found');
+            return this.canvas.toDataURL();
+        }
+    }
+})();
---
+++ public/js/index.js
@@ -0,0 +1,108 @@
+var readUploadedFile = function(file, callback) {
+    var reader = new FileReader();
+    reader.onload = function(event) {
+        var imgObj = new Image();
+        imgObj.src = event.target.result;
+        imgObj.onload = function() {
+            callback(imgObj)
+        }
+    }
+    reader.readAsDataURL(file);
+}
+
+const cloudName = 'dsoxr0nis';
+const unsignedUploadPreset = 'hxay11ox';
+
+// *********** Upload file to Cloudinary ******************** //
+function uploadFile(file) {
+    var url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
+    var xhr = new XMLHttpRequest();
+    var fd = new FormData();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+    // Reset the upload progress bar
+    document.getElementById('progress').style.width = 0;
+
+    // Update progress (can be used to show progress indicator)
+    xhr.upload.addEventListener("progress", function(e) {
+        var progress = Math.round((e.loaded * 100.0) / e.total);
+        document.getElementById('progress').style.width = progress + "%";
+    });
+
+    xhr.onreadystatechange = function(e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // File uploaded successfully
+            var response = JSON.parse(xhr.responseText);
+            // https://res.cloudinary.com/cloudName/image/upload/v1483481128/public_id.jpg
+            var url = response.secure_url;
+            // Create a thumbnail of the uploaded image, with 150px width
+            var tokens = url.split('/');
+            tokens.splice(-2, 0, 'w_150,c_scale');
+            var img = new Image(); // HTML5 Constructor
+            img.src = tokens.join('/');
+            img.alt = response.public_id;
+            document.getElementById('gallery').appendChild(img);
+        }
+    };
+
+    fd.append('upload_preset', unsignedUploadPreset);
+    fd.append('tags', 'browser_upload'); // Optional - add tag for image admin in Cloudinary
+    fd.append('file', file);
+    xhr.send(fd);
+}
+
+function dataURLtoFile(dataurl, filename) {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, {type:mime});
+}
+
+//Usage example:
+var file = dataURLtoFile('data:text/plain;base64,aGVsbG8gd29ybGQ=', 'hello.txt');
+
+
+$(document).ready(function() {
+
+    //intialize design studio by creating canvas
+    DesignStudio.createCanvas('canvas');
+
+    //add text handler
+    $('#addText').on('click', function() {
+        DesignStudio.addText($('#textInput').val());
+    });
+
+    //add image handler
+    $('#addImage').on('change', function(e) {
+        readUploadedFile(e.target.files[0], function(img) {
+            DesignStudio.addImage(img);
+        });
+    })
+
+    //set canvas background (product image)
+    $('#addBackground').on('change', function(e) {
+        readUploadedFile(e.target.files[0], function(img) {
+            DesignStudio.setBackground(img);
+        });
+    });
+
+    //delete the active/selected object
+    $('#deleteObj').on('click', function(e) {
+        DesignStudio.deleteActiveObject();
+    });
+
+    //clear designs handler
+    $('#clearDesigns').on('click', function(e) {
+        DesignStudio.clearDesigns();
+    });
+
+    //export final design
+    $('#export').on('click', function(e) {
+    	var file = dataURLtoFile(DesignStudio.exportDesign(), 'temp.png');
+        uploadFile(file)
+    })
+
+})
